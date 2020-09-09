40f5b9e0eeb16112af82b8237d1875962ac7f896
Arthur-lz <twuufnxlz@163.com>
2020-09-10 04:37:22 +0800

    add dir test, add saybymyword for linux study

--- test/book1.c
+++ test/book1.c
@@ -3,20 +3,30 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main()
+int main(int argc, char *argv[])
 {
 	int num = 1;
-	pid_t pid = vfork();
+	pid_t pid = fork();
 	if(pid < 0)
 	{
 		perror("vfork error");
 	}
-	else if(pid == 0)
+	else if(pid == 0) //当前在子进程中
 	{
-		printf("num is %d, child is: %d\n", ++num, getpid());
+		printf("child exit, num is %d, child is: %d, parent is: %d\n", ++num, getpid(), getppid());
+		int i;
+		int count = 100;
+		if (argc == 1) {
+			count = (int)argv[0];	
+		}
+		
+		printf("count is %d\n", count);
+		for (i = 0; i < count; i++) {
+			printf("%d\n",i);
+		}	
 		exit(0);
 	}
-	else 
+	else// pid > 0，当前在父进程中 
 	{
 		printf("num is %d, parent is: %d\n", ++num, getpid());
 		exit(0);
